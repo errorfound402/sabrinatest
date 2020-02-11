802b9d2f6929f9a4bb2e42c826c551a1fa52d0ff
randomkanger <60593860+randomkanger@users.noreply.github.com>
2020-02-11 23:39:41 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -48,7 +48,7 @@ if ENV:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    URL = os.environ.get('URL', "postgres://postgres:aeiou.123@database-1.c3sjxf39swvc.us-east-2.rds.amazonaws.com:5432/database-1")  # Does not contain token
+    URL = os.environ.get('URL', " ")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5432))
     CERT_PATH = os.environ.get("CERT_PATH")
 
